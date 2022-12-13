aa5eb658cdab0a8262f5a9348f188fa122c1a02c
mfranz <franz08@email.franklin.edu>
2022-12-13 09:47:27 -0500

    Improve memory and efficiency by using 26 letters of the alphabet and using the list set as seen

--- hard/127-word-ladder.js
+++ hard/127-word-ladder.js
@@ -8,25 +8,11 @@
 
     // store in set, to check if it exists o(1)
     const list = new Set([...wordList])
-
-    // store all possible letter options under each index
-    const letters = new Map()
-    for (const word of wordList) {
-        for (let i = 0; i < word.length; i++) {
-            const curr = word.charAt(i)
-            const strI = i.toString()
-
-            if (letters.has(strI)) {
-                letters.set(strI, letters.get(strI).add(curr))
-            } else {
-                letters.set(strI, new Set([curr]))
-            }
-        }
-    }
+    const alphabet = "abcdefghijklmnopqrstuvwxyz";
 
     let queue = []
     queue.push([beginWord, 1])
-    let seen = new Set([beginWord])
+    list.delete(beginWord)
 
     while (queue.length > 0) {
         let [word, count] = queue.shift()
@@ -35,14 +21,12 @@
 
         for (let i = 0; i < word.length; i++) {
 
-            const lettersByIdx = letters.get(i.toString())
-
-            for (letter of lettersByIdx) {
+            for (letter of alphabet) {
                 const option = word.substring(0, i) + letter + word.substring(i + 1)
                 
-                if (list.has(option) && !seen.has(option)) {
+                if (list.has(option)) {
                     queue.push([option, count+1])
-                    seen.add(option)
+                    list.delete(option)
                 }
             }
 
